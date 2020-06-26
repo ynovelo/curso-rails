e1b7e761b9f8117c4685c633435584192173c1d5
ynovelo <ynovelo23@gmail.com>
2020-06-26 00:22:55 -0500

    change the index page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,6 @@ Rails.application.routes.draw do
 
   #get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'welcome#index'
 
 end
