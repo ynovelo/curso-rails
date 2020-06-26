adb89cccdb71b80e0c98df4a1df423287392b56e
ynovelo <ynovelo23@gmail.com>
2020-06-25 23:54:16 -0500

    Create static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   devise_for :users
   resources :articles
 
@@ -19,6 +20,6 @@ Rails.application.routes.draw do
 
   #get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'pages#index'
 
 end
